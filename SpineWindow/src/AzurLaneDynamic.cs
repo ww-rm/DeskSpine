d3ef96bb2c215eecabc0ae030f7424363f0549a9
ww-rm <43590561+ww-rm@users.noreply.github.com>
2024-08-28 13:47:43 +0800

    Update AzurLaneDynamic.cs

--- SpineWindow/src/AzurLaneDynamic.cs
+++ SpineWindow/src/AzurLaneDynamic.cs
@@ -1,4 +1,3 @@
-﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +5,21 @@ using System.Threading.Tasks;
 
 namespace SpineWindow
 {
-    public class AzurLaneDynamic
+    public class AzurLaneDynamic : SpineWindow
     {
+        private string animation_T = "normal";
+        private string animation_M = "normal";
+        private string animation_B = "normal";
 
+        protected override void Trigger_SpineLoaded(uint index)
+        {
+            base.Trigger_SpineLoaded(index);
+
+            mutex.WaitOne();
+            if (spineSlots[0] is not null) spineSlots[0].CurrentAnimation = animation_T;
+            if (spineSlots[1] is not null) spineSlots[1].CurrentAnimation = animation_M;
+            if (spineSlots[2] is not null) spineSlots[2].CurrentAnimation = animation_B;
+            mutex.ReleaseMutex();
+        }
     }
 }
