8193b6d0227aa57de2c9dbbe0e56f59270c61bcc
ww-rm <ww-rm@qq.com>
2024-08-30 21:42:59 +0800

    遍历设置

--- SpineWindow/src/AzurLaneDynamic.cs
+++ SpineWindow/src/AzurLaneDynamic.cs
@@ -7,18 +7,12 @@ namespace SpineWindow
 {
     public class AzurLaneDynamic : SpineWindow
     {
-        private string animation_T = "normal";
-        private string animation_M = "normal";
-        private string animation_B = "normal";
-
         protected override void Trigger_SpineLoaded(uint index)
         {
             base.Trigger_SpineLoaded(index);
 
             mutex.WaitOne();
-            if (spineSlots[0] is not null) spineSlots[0].CurrentAnimation = animation_T;
-            if (spineSlots[1] is not null) spineSlots[1].CurrentAnimation = animation_M;
-            if (spineSlots[2] is not null) spineSlots[2].CurrentAnimation = animation_B;
+            foreach (var sp in spineSlots) { if (sp is not null) sp.CurrentAnimation = "normal"; }
             mutex.ReleaseMutex();
         }
     }
