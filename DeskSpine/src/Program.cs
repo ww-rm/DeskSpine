86d2082c7ee9c3a1e0c51ad496b39399c7aebc50
ww-rm <ww-rm@qq.com>
2024-09-01 23:57:01 +0800

    增加开机自启属性

--- DeskSpine/src/Program.cs
+++ DeskSpine/src/Program.cs
@@ -1,10 +1,37 @@
+using Microsoft.Win32;
+
 namespace DeskSpine
 {
     public static class Program
     {
+        public const string ProgramName = "DeskSpine";
         public static ConfigForm configForm;
         public static SpineWindow.SpineWindow spineWindow;
-        public static object debugobj;
+        public static Mutex programMutex;
+
+
+        public static bool AutoRun
+        {
+            get
+            {
+                var exePath = Application.ExecutablePath;
+                var ret = false;
+                using (RegistryKey runKey = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run"))
+                    ret = runKey.GetValue(ProgramName, "").ToString() == exePath;
+                return ret;
+            }
+            set
+            {
+                var exePath = Application.ExecutablePath;
+                using (RegistryKey runKey = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run"))
+                {
+                    if (value)
+                        runKey.SetValue(ProgramName, exePath);
+                    else
+                        runKey.DeleteValue(ProgramName, false);
+                }
+            }
+        }
 
         /// <summary>
         ///  The main entry point for the application.
@@ -12,6 +39,17 @@ namespace DeskSpine
         [STAThread]
         public static void Main()
         {
+            bool createNew;
+            programMutex = new Mutex(true, "DeskSpine", out createNew);
+
+#if !DEBUG
+            if (!createNew)
+            {
+                MessageBox.Show("程序已在运行, 请勿重复启动", "DeskSpine");
+                return;
+            }
+#endif
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
