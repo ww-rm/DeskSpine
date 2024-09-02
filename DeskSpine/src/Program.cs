93c5f8eedd70b20f5b88d50b60f8614f232f6f50
ww-rm <ww-rm@qq.com>
2024-09-02 21:43:43 +0800

    增加GetCurrentConfig

--- DeskSpine/src/Program.cs
+++ DeskSpine/src/Program.cs
@@ -44,13 +44,13 @@ namespace DeskSpine
         {
             programMutex = new Mutex(true, ProgramName, out bool createNew);
 
-#if !DEBUG
+            #if !DEBUG
             if (!createNew)
             {
                 MessageBox.Show("程序已在运行, 请勿重复启动", ProgramName);
                 return;
             }
-#endif
+            #endif
 
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
@@ -63,6 +63,9 @@ namespace DeskSpine
             Application.Run();
         }
 
+        /// <summary>
+        /// 从本地配置文件进行初始化
+        /// </summary>
         private static void InitFromLocalConfig()
         {
             var localConfig = new Config();
@@ -80,6 +83,8 @@ namespace DeskSpine
             spineWindow.Opacity = localConfig.BasicConfig.Opacity;
             spineWindow.MaxFps = localConfig.BasicConfig.MaxFps;
             spineWindow.BackgroudColor = localConfig.BasicConfig.BackgroudColor;
+
+            // 不需要管位置和大小信息, 会通过注册表加载
 
             // 加载 Spine 资源
             var spVersion = localConfig.BasicConfig.SpineVersion;
@@ -101,6 +106,44 @@ namespace DeskSpine
 
             // 设置窗口可见状态
             spineWindow.Visible = localConfig.SystemConfig.Visible;
+        }
+
+        /// <summary>
+        /// 根据程序实时运行状态获取配置
+        /// </summary>
+        /// <returns></returns>
+        public static Config GetCurrentConfig()
+        {
+            var config = new Config();
+            config.SystemConfig.AutuRun = AutoRun;
+            config.SystemConfig.Visible = spineWindow.Visible;
+            config.BasicConfig.WallpaperMode = spineWindow.WallpaperMode;
+            config.BasicConfig.MouseClickThrough = spineWindow.MouseClickThrough;
+            config.BasicConfig.SpineScale = spineWindow.SpineScale;
+            config.BasicConfig.SpineFlip = spineWindow.SpineFlip;
+            config.BasicConfig.Opacity = spineWindow.Opacity;
+            config.BasicConfig.MaxFps = spineWindow.MaxFps;
+            config.BasicConfig.BackgroudColor = spineWindow.BackgroudColor;
+            config.BasicConfig.WindowType = spineWindow.Type;
+            config.BasicConfig.SpineVersion = spineWindow.SpineVersion;
+            for (int i = 0; i < spineWindow.SlotCount; i++)
+            {
+                config.SpineConfig.SetSkelPath(i, spineWindow.GetSpineSkelPath(i));
+            }
+
+            // 一些临时量
+            var position = spineWindow.Position;
+            config.BasicConfig.PositionX = position.X;
+            config.BasicConfig.PositionY = position.Y;
+            var size = spineWindow.Size;
+            config.BasicConfig.SizeX = size.X;
+            config.BasicConfig.SizeY = size.Y;
+            var spPosition = spineWindow.Position;
+            config.BasicConfig.SpinePositionX = spPosition.X;
+            config.BasicConfig.SpinePositionY = spPosition.Y;
+            config.BasicConfig.ClearColor = spineWindow.ClearColor;
+
+            return config;
         }
     }
 }
