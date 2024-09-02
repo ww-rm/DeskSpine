a939ea43c8221f22630b044b7673c026ca31e9eb
ww-rm <ww-rm@qq.com>
2024-09-02 21:02:03 +0800

    增加配置类

---
+++ DeskSpine/src/Config.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DeskSpine
+{
+    /// <summary>
+    /// 系统设置
+    /// </summary>
+    public class SystemConfig
+    {
+        public bool AutuRun { get; set; } = false;
+        public bool Visible { get; set; } = true;
+    }
+
+    /// <summary>
+    /// 基础设置
+    /// </summary>
+    public class BasicConfig
+    {
+        public bool WallpaperMode { get; set; } = false;
+        public bool MouseClickThrough { get; set; } = false;
+        public float SpineScale { get; set; } = 1f;
+        public bool SpineFlip { get; set; } = false;
+        public byte Opacity { get; set; } = 255;
+        public uint MaxFps { get; set; } = 30;
+        public SpineWindow.BackgroudColor BackgroudColor { get; set; } = SpineWindow.BackgroudColor.Gray;
+        public SpineWindow.SpineWindowType WindowType { get; set; } = SpineWindow.SpineWindowType.AzurLaneSD;
+        public string SpineVersion { get; set; } = "3.8.x";
+    }
+
+    /// <summary>
+    /// Spine 设置
+    /// </summary>
+    public class SpineConfig
+    {
+        public const int SlotCount = 10;
+        private string?[] skelPaths = new string?[SlotCount];
+
+        public string? SkelPath0 { get => skelPaths[0]; set => skelPaths[0] = value; }
+        public string? SkelPath1 { get => skelPaths[1]; set => skelPaths[1] = value; }
+        public string? SkelPath2 { get => skelPaths[2]; set => skelPaths[2] = value; }
+        public string? SkelPath3 { get => skelPaths[3]; set => skelPaths[3] = value; }
+        public string? SkelPath4 { get => skelPaths[4]; set => skelPaths[4] = value; }
+        public string? SkelPath5 { get => skelPaths[5]; set => skelPaths[5] = value; }
+        public string? SkelPath6 { get => skelPaths[6]; set => skelPaths[6] = value; }
+        public string? SkelPath7 { get => skelPaths[7]; set => skelPaths[7] = value; }
+        public string? SkelPath8 { get => skelPaths[8]; set => skelPaths[8] = value; }
+        public string? SkelPath9 { get => skelPaths[9]; set => skelPaths[9] = value; }
+
+        public string? GetSkelPath(int index)
+        {
+            if (index > SlotCount) throw new ArgumentOutOfRangeException(nameof(index), $"Index must less than {SlotCount}");
+            var propertyName = $"SkelPath{index}";
+            var propertyInfo = typeof(SpineConfig).GetProperty(propertyName);
+            return propertyInfo?.GetValue(this) as string;
+        }
+    }
+
+    /// <summary>
+    /// DeskSpine 设置
+    /// </summary>
+    public class Config
+    {
+        private static JsonSerializerOptions jsonOptions = new() { WriteIndented = true };
+
+        public SystemConfig SystemConfig { get; set; } = new();
+        public BasicConfig BasicConfig { get; set; } = new();
+        public SpineConfig SpineConfig { get; set; } = new();
+
+        public Config()
+        {
+            // 给一个默认的路径
+            SpineConfig.SkelPath0 = Path.Combine(Program.ProgramDirectory, @"res\spines\AzurLaneSD\guanghui_2.skel");
+        }
+
+        public bool Load(string configPath)
+        {
+            if (File.Exists(configPath))
+            {
+                try
+                {
+                    string jsonString = File.ReadAllText(configPath);
+                    var config = JsonSerializer.Deserialize<Config>(jsonString);
+
+                    if (config != null)
+                    {
+                        if (config.SystemConfig is not null) SystemConfig = config.SystemConfig;
+                        if (config.BasicConfig is not null) BasicConfig = config.BasicConfig;
+                        if (config.SpineConfig is not null) SpineConfig = config.SpineConfig;
+                    }
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error reading config file: {ex.Message}");
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        public bool Save(string configPath)
+        {
+            try
+            {
+                string jsonString = JsonSerializer.Serialize(this, jsonOptions);
+                File.WriteAllText(configPath, jsonString);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error saving config file: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
--- DeskSpine/src/Program.cs
+++ DeskSpine/src/Program.cs
@@ -1,14 +1,20 @@
 using Microsoft.Win32;
+using SpineWindow;
 
 namespace DeskSpine
 {
     public static class Program
     {
         public const string ProgramName = "DeskSpine";
-        public static ConfigForm configForm;
-        public static SpineWindow.SpineWindow spineWindow;
-        public static Mutex programMutex;
+        public static string LocalAppdataDirectory { get; } = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        public static string ProgramDirectory { get; } = Path.GetDirectoryName(Application.ExecutablePath);
+        public static string ProgramDataDirectory { get; } = Path.Combine(LocalAppdataDirectory, ProgramName);
+        public static string ProgramConfigPath { get; } = Path.Combine(ProgramDataDirectory, "config.json");
 
+        private static ConfigForm configForm;
+        private static SpineWindow.SpineWindow spineWindow;
+
+        private static Mutex programMutex;
 
         public static bool AutoRun
         {
@@ -33,19 +39,15 @@ namespace DeskSpine
             }
         }
 
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         public static void Main()
         {
-            bool createNew;
-            programMutex = new Mutex(true, "DeskSpine", out createNew);
+            programMutex = new Mutex(true, ProgramName, out bool createNew);
 
 #if !DEBUG
             if (!createNew)
             {
-                MessageBox.Show("绋嬪簭宸插湪杩愯, 璇峰嬁閲嶅鍚姩", "DeskSpine");
+                MessageBox.Show("程序已在运行, 请勿重复启动", ProgramName);
                 return;
             }
 #endif
@@ -53,12 +55,52 @@ namespace DeskSpine
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            configForm = new ConfigForm();
 
-            spineWindow = new SpineWindow.AzurLaneSD();
-            spineWindow.LoadSpine("3.6", "D:\\Program Files\\DesktopSprite\\res\\spine\\guanghui_2.skel");
-            spineWindow.Visible = true;
+            if (!Directory.Exists(ProgramDataDirectory))
+                Directory.CreateDirectory(ProgramDataDirectory);
+            InitFromLocalConfig();
+            configForm = new ConfigForm();
             Application.Run();
+        }
+
+        private static void InitFromLocalConfig()
+        {
+            var localConfig = new Config();
+            localConfig.Load(ProgramConfigPath);
+
+            // 系统配置
+            AutoRun = localConfig.SystemConfig.AutuRun;
+
+            // 基础配置
+            spineWindow = SpineWindow.SpineWindow.New(localConfig.BasicConfig.WindowType, SpineConfig.SlotCount);
+            spineWindow.WallpaperMode = localConfig.BasicConfig.WallpaperMode;
+            spineWindow.MouseClickThrough = localConfig.BasicConfig.MouseClickThrough;
+            spineWindow.SpineFlip = localConfig.BasicConfig.SpineFlip;
+            spineWindow.SpineScale = localConfig.BasicConfig.SpineScale;
+            spineWindow.Opacity = localConfig.BasicConfig.Opacity;
+            spineWindow.MaxFps = localConfig.BasicConfig.MaxFps;
+            spineWindow.BackgroudColor = localConfig.BasicConfig.BackgroudColor;
+
+            // 加载 Spine 资源
+            var spVersion = localConfig.BasicConfig.SpineVersion;
+            for (int i = 0; i < spineWindow.SlotCount; i++)
+            {
+                var skelPath = localConfig.SpineConfig.GetSkelPath(i);
+                if (skelPath is not null)
+                {
+                    try
+                    { 
+                        spineWindow.LoadSpine(spVersion, skelPath, index: i); 
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"{skelPath} 加载失败\n\n{ex}", ProgramName);
+                    }
+                }
+            }
+
+            // 设置窗口可见状态
+            spineWindow.Visible = localConfig.SystemConfig.Visible;
         }
     }
 }
