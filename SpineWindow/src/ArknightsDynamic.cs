fb1627d7599b7687be4d23f31c4eece2318cb659
ww-rm <ww-rm@qq.com>
2024-09-04 10:32:27 +0800

    增加方舟动绘窗口

--- SpineWindow/src/ArknightsDynamic.cs
+++ SpineWindow/src/ArknightsDynamic.cs
@@ -1,12 +1,69 @@
-﻿using System;
+﻿using SFML.Window;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SpineWindow
 {
-    internal class ArknightsDynamic
+    public class ArknightsDynamic : SpineWindow
     {
+        private string animation_Idle = "Idle";
+        private string animation_Interact = "Interact";
+        private string animation_Special = "Special";
+
+        private Random rnd = new();
+
+        private SFML.System.Clock clockSpecial = new();
+        private const float meanWaitingTime = 30f;
+        private float nextSpecialTime = 10f;
+
+        public ArknightsDynamic(uint slotCount) : base(slotCount) { }
+
+        protected override void Trigger_SpineLoaded(int index)
+        {
+            base.Trigger_SpineLoaded(index);
+
+            mutex.WaitOne();
+            if (spineSlots[0] is not null) 
+                spineSlots[0].CurrentAnimation = animation_Idle;
+            mutex.ReleaseMutex();
+        }
+
+        protected override void Trigger_StateUpdated()
+        {
+            base.Trigger_StateUpdated();
+
+            if (clockSpecial.ElapsedTime.AsSeconds() >= nextSpecialTime)
+            {
+                clockSpecial.Restart();
+                nextSpecialTime = (float)(-meanWaitingTime * Math.Log(rnd.NextSingle()));
+                Debug.WriteLine($"Next time to special: {nextSpecialTime}");
+                mutex.WaitOne();
+                if (spineSlots[0] is not null)
+                {
+                    if (spineSlots[0].CurrentAnimation == animation_Idle)
+                    {
+                        spineSlots[0].CurrentAnimation = animation_Special;
+                        spineSlots[0].AddAnimation(animation_Idle);
+                    }
+                }
+                mutex.ReleaseMutex();
+            }
+        }
+
+        protected override void Trigger_MouseButtonClick(MouseButtonEventArgs e)
+        {
+            base.Trigger_MouseButtonClick(e);
+            mutex.WaitOne();
+            if (spineSlots[0] is not null && spineSlots[0].CurrentAnimation != animation_Interact)
+            {
+                spineSlots[0].CurrentAnimation = animation_Interact;
+                spineSlots[0].AddAnimation(animation_Idle);
+            }
+            mutex.ReleaseMutex();
+        }
     }
 }
